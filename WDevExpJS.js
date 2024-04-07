672e2628cb34946a50060cbf9b8bebc070caa885
Wilber Jose Matus Gonzalez <wilberj1987@gmail.com>
2024-04-07 11:38:31 -0600

    profiles-ext/i-sadav#2499 correción  en emisión de vueling NDC

--- WDevExpJS.js
+++ WDevExpJS.js
@@ -1,22 +1,21 @@
 import { html, WRender } from "./WModules/WComponentsTools.js";
 import { css } from "./WModules/WStyledRender.js";
 
-window.onload = async () => {    
+window.onload = async () => {
     linksContainer.append(html`<h1>Analitic Components</h1>`);
-    await buildLinks("AnaliticComponents/");
+    await buildLinks("AnaliticComponents/", ["Bar_Charts", "Dinamic_Table", "Radial_Charts"]);
     linksContainer.append(html`<h1>Form Components</h1>`);
-    await buildLinks("FormElements/");
+    await buildLinks("FormElements/", ["Basic_Table"]);
 }
 
-async function buildLinks(path) {
+async function buildLinks(path, pathList = []) {
     const route = "./WComponents/Examples/" + path;
-    const dir = await fetch(route);
-    const response = await dir.text();
-    const container = WRender.Create({ innerHTML: response });
-    const links = container.querySelectorAll("a.icon-html");   
-    links.forEach(element => {
-        const name = element.title.replace(".html", "")
-            linksContainer.append(css`
+    //const dir = await fetch(route);
+    //const response = await dir.text();
+    //const container = WRender.Create({ innerHTML: response });
+    //const links = container.querySelectorAll("a.icon-html");   
+    pathList.forEach(name => {
+        linksContainer.append(css`
             .site-header .${name} {                
                 height: 400px;
                 width: 700px;
@@ -34,9 +33,9 @@ async function buildLinks(path) {
             }
         `)
         linksContainer.append(WRender.Create({
-            tagName: 'a', className: 'a'+name, innerText: name.replace("_",  " "), href: route + element.title
+            tagName: 'a', className: 'a' + name, innerText: name.replace("_", " "), href: route + name + ".html"
         }));
-        linksContainer.append(html`<div class="preview ${name}" style='color: #000'><img src="./Media/ImageComponents/${name}.png"/></div>`)
-       
+        linksContainer.append(html`<div class="preview ${name}" style='color: #000'><img src="./Media/ImageComponents/${name}.png" /></div>`)
+
     });
 }
