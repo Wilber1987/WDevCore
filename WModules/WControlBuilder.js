f44b9c29eb0ca65b835c15565ffdfed164f4f401
wilber jose matus <wilberj1987@gmail.com>
2024-07-06 10:00:45 -0600

    profiles-ext/i-sadav#2978 ajustes en recibos

--- WModules/WControlBuilder.js
+++ WModules/WControlBuilder.js
@@ -1,27 +1,27 @@
-import { WRender } from "./WComponentsTools.js";
+import { WRender, html } from "./WComponentsTools.js";
 
 class ControlBuilder {
-    static BuildImage(value = "", urlPath) {
-        let cadenaB64 = "";
-        var base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
-        if (base64regex.test(value.replace("data:image/png;base64,", ""))) {
-            cadenaB64 = !value.includes("data:image/png;base64,") ? "data:image/png;base64," : "";
-        } else if (urlPath != undefined && urlPath != "") {
-            cadenaB64 = urlPath + "/";
-        }
-        const image = WRender.createElement({
-            type: "img",
-            props: {
-                src: cadenaB64 + value,
-                class: "imgPhoto",
-                height: 50,
-                width: 50
-            }
-        });
-        return image;
+  static BuildImage(value = "", urlPath) {
+    let cadenaB64 = "";
+    var base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+    if (base64regex.test(value.replace("data:image/png;base64,", ""))) {
+      cadenaB64 = !value.includes("data:image/png;base64,") ? "data:image/png;base64," : "";
+    } else if (urlPath != undefined && urlPath != "") {
+      cadenaB64 = urlPath + "/";
     }
-    static BuildProgressBar(value = 0, total = 100, barColor = "#737b83cc", progressColor = "#2c3e50") {
-        return WRender.CreateStringNode(`<div class="progressDivContainer">   
+    const image = WRender.createElement({
+      type: "img",
+      props: {
+        src: cadenaB64 + value,
+        class: "imgPhoto",
+        height: 50,
+        width: 50
+      }
+    });
+    return image;
+  }
+  static BuildProgressBar(value = 0, total = 100, barColor = "#737b83cc", progressColor = "#2c3e50") {
+    return html`<div class="progressDivContainer">   
         <h3>${(value / (total > 0 ? total : 1) * 100).toFixed(2)}%</h3>     
         <div class="progressDiv">        
           <div class="progress" style="width:${(value / (total > 0 ? total : 1) * 100).toFixed(2)}%;"></div>
@@ -40,7 +40,7 @@ class ControlBuilder {
             margin: 2px 10px; font-size: 16px; font-family: sans-serif; color: #fff; position: absolute; top: 0
           }
         </style>
-      </div>`);
-    }
+      </div>`;
+  }
 }
 export { ControlBuilder }
