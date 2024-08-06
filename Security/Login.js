e9a04e99dc9d7e689b872be92eee2f8093bb45f9
Cesar Mejia <cesarcalderon909@gmail.com>
2024-08-06 09:01:49 -0600

    ---

--- Security/Login.js
+++ Security/Login.js
@@ -13,37 +13,72 @@ const OnLoad = async () => {
 
     setTimeout(() => {
         const LoginForm = GetLoginLayout(WRender.Create({
-            className: "sign-in-form", children: [
-                { tagName: 'h3', innerText: "Inicio de sesión", class: 'title' },
-                {
-                    class: "mb-3", children: [
-                        html`<img src="/WDevCore/Security/Styles/img/user.png" class="form-control" alt="" />`, {
-                            tagName: 'input', type: 'text', placeholder: 'Correo', onchange: (ev) => {                                UserData.user = ev.target.value;
+            className: "sign-in-form",
+            children: [{
+                tagName: 'h3',
+                innerText: "Inicio de sesión",
+                class: 'title'
+            }, {
+                class: "mb-3",
+                children: [{
+                    class: 'position-relative input-custom-icon',
+                    children: [
+                        html `<span class="bx bx-user"></span>`,
+                        {
+                            id: 'username',
+                            tagName: 'input',
+                            type: 'text',
+                            placeholder: 'Usuario', 
+                            className: 'form-control',
+                            onchange: (ev) => {
+                                UserData.user = ev.target.value;
                             }
-                        }]
-                }, {
-                    class: "mb-3", children: [
-                        html`<label class="form-label" for="username">Password</label>`,{
-                            class: "position-relative input-custom-icon", children: [
-                                {
-                                    tagName: 'input', type: 'password', placeholder: 'Contraseña', className: 'form-control', onchange: (ev) => {
-                                    UserData.password = ev.target.value;
-                                }
-                            }]
-                        }    
+                        }
                     ]
-                }, {
-
-                    tagName: 'input', type: 'button', className: 'btn btn-primary', value: 'Ok',
+                }]
+            }, {
+                class: "mb-3",
+                children: [{
+                    class: "position-relative auth-pass-inputgroup input-custom-icon",
+                    children: [
+                        html `<span class="bx bx-lock-alt"></span>`, {
+                            tagName: 'input',
+                            id: "password-input",
+                            type: 'password',
+                            placeholder: 'Contraseña',
+                            className: 'form-control',
+                            onchange: (ev) => {
+                                UserData.password = ev.target.value;
+                            },
+                            children: [{
+                                id: "password-addon",
+                                tagName: 'button',
+                                type: 'button',
+                                className: 'btn btn-link position-absolute h-100 end-0 top-0',
+                                children: [
+                                    html `<i class="mdi mdi-eye-outline font-size-18 text-muted"></i>`
+                                ]
+                            }]
+                        }
+                    ]
+                }]
+            }, {
+                class: 'mt-3',
+                children: [{
+                    tagName: 'input',
+                    type: 'button',
+                    className: 'btn btn-primary w-100 waves-effect waves-light',
+                    value: 'Ok',
                     onclick: async () => WSecurity.Login(UserData)
-
-                }, {
-                    children: [{
-                        tagName: 'a', innerText: 'Recuperar contraseña', href: "/Security/RecoveryPassword"
-                    }]
-                }
-            ]
-        }), html`<form></form>`)
+                }]
+            }, {
+                children: [{
+                    tagName: 'a',
+                    innerText: 'Recuperar contraseña',
+                    href: "/Security/RecoveryPassword"
+                }]
+            }]
+        }), html `<form></form>`)
         // @ts-ignore
         App.appendChild(LoginForm);
     }, 300);
@@ -56,14 +91,39 @@ const OnLoad = async () => {
 * @returns {HTMLElement}
 */
 const GetLoginLayout = (loginForm, registerForm) => {
-    const loginLayout = html`<div class="container">        
-        <div class="forms-container">
-            <div class="signin-signup" id="container">                
-                    
-            </div>
-        </div>
+    const loginLayout = html`<div class="authentication-bg min-vh-100">
+        <div class="bg-overlay bg-light"></div>
+        <div class="container">
+            <div class="d-flex flex-column min-vh-100 px-3">
+                <div class="row justify-content-center my-auto">
+                    <div class="col-md-8 col-lg-6 col-xl-5">
 
-        <!-- <div class="panels-container">
+                        <div class="mb-4 pb-2">
+                            <a href="index.html" class="d-block auth-logo">
+                                <img src="/Media/img/logotipoCCA.png" alt="" height="150" class="auth-logo-dark me-start">
+                                <img src="/Media/img/logotipoCCA.png" alt="" height="150" class="auth-logo-light me-start">
+                            </a>
+                        </div>
+
+                        <div class="card">
+                            <div class="card-body p-4"> 
+                                <div class="text-center mt-2">
+                                    <h3>${localStorage.getItem("TITULO")}</h3>
+                                    <p>${localStorage.getItem("SUB_TITULO")}</p>
+                                </div>
+                                <div class="p-2 mt-4" id="container">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- end container -->
+    </div>
+    
+    
+     <!-- <div class="panels-container">
             <div class="panel left-panel">
                 <div class="content">
                     <img class="logo" src="/Media/img/logotipoCCA.png"/>
@@ -90,49 +150,11 @@ const GetLoginLayout = (loginForm, registerForm) => {
                 </div>
                 <img src="/WDevCore/Security/Styles/img/register.svg" class="image" alt="" />
             </div>
-        </div> -->
-    </div>`;
+        </div> -->`;
     loginLayout.querySelector("#container")?.append(loginForm);
     return loginLayout;
 
 }
-const MasterStyle = css`
-       /* body {
-            font-family: "Poppins", sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 90vh;
-        }
 
-        .LoginForm {
-            width: 450px;
-            overflow: hidden;
-            padding: 30px;
-            box-shadow: 0 0 2px rgba(0, 0, 0, 0.4);
-            border-radius: 30px;
-            display: flex;
-            justify-content: center;
-            flex-direction: column;
-            align-items: center;
-        }
 
-        .LoginForm div {
-            width: 100%;
-            text-align: center;
-            margin-bottom: 10px;
-            margin-top: 10px;
-        }
-
-        img {
-            display: block;
-            width: 50%;
-        }
-
-        @media (max-width: 800px) {
-            .LoginForm {
-                box-shadow: none;
-            }
-        }*/
-        `;
 window.onload = OnLoad;
