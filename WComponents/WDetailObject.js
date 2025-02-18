48b9a1e213b4dea4e5358d4217fc213dd5062b65
Wilber jose matus <wilberj@gmail.com>
2025-02-18 10:16:02 -0600

    profiles-ext/i-sadav#3967 ajustes en detalles de factura

--- WComponents/WDetailObject.js
+++ WComponents/WDetailObject.js
@@ -4,7 +4,6 @@ import { ComponentsManager, ConvertToMoneyString, WRender } from '../WModules/WC
 import { ControlBuilder } from '../WModules/WControlBuilder.js';
 import { WOrtograficValidation } from '../WModules/WOrtograficValidation.js';
 import { css, WCssClass, WStyledRender } from '../WModules/WStyledRender.js';
-import { WAcorden } from "./WAcordeon.js";
 import { WAppNavigator } from './WAppNavigator.js';
 import { WTableComponent } from './WTableComponent.js';
 let photoB64;
@@ -108,12 +107,7 @@ class WDetailObject extends HTMLElement {
                                 ObjectDetail[prop] = ObjectDetail[prop] != ""
                                     && ObjectDetail[prop] != undefined
                                     && ObjectDetail[prop] != null
-                                    && ObjectDetail[prop].__proto__ == Array.prototype ? ObjectDetail[prop] : [];
-                                /*return new WAcorden({
-                                    ModelObject: Model[prop].ModelObject.__proto__ == Function.prototype ? Model[prop].ModelObject() : Model[prop].ModelObject,
-                                    Dataset: ObjectDetail[prop] ?? []
-                                });*/
-
+                                    && ObjectDetail[prop].__proto__ == Array.prototype ? ObjectDetail[prop] : [];   
                                 return new WTableComponent({
                                     Options: { Search: true, Show: true },
                                     ImageUrlPath: this.Config.ImageUrlPath,
